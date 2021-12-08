83008ba7ee6cfd8101580e3b267d18472306ce4f
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-07 22:50:53 -0600

    add image to app.js

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import CalendarEvents from './CalendarEvents';
 import Footer from './Footer';
 import Header from './Header';
+import Logo from './Logo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Logo />
       <CalendarEvents />
       <Footer />
       <StatusBar style="auto" />
